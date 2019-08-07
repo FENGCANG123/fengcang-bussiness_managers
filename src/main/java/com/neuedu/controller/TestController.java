8ee19d2f58d0b4414991fe079862ca7a92bef206
FENGCANG <18712769206@163.COM>
2019-08-07 19:56:16 +0800

    完成图片上传功能

--- src/main/java/com/neuedu/controller/TestController.java
+++ src/main/java/com/neuedu/controller/TestController.java
@@ -2,16 +2,42 @@ package com.neuedu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class TestController {
 
 
-    @RequestMapping(value = "login")
+    @RequestMapping(value = "upload",method = RequestMethod.GET)
     public  String  login(){
 
-        return "login";
+        return "upload";
+    }
+    @RequestMapping(value = "upload",method = RequestMethod.POST)
+    public  String  login(@RequestParam("pic")MultipartFile uploadFile){
+if (uploadFile!=null)
+{
+    String uuid= UUID.randomUUID().toString();
+    String fileName=uploadFile.getOriginalFilename();
+    String fileextendname=fileName.substring(fileName.lastIndexOf("."));
+    String newFileName=uuid+fileextendname;
+    File file=new File("D:\\upload");
+    File newFile=new File(file,newFileName);
+    try {
+        uploadFile.transferTo(newFile);
+    } catch (IOException e) {
+        e.printStackTrace();
     }
 
+
+}
+        return "upload";
+    }
 }
 
